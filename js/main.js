2d25f98558063f14b662e6bb8d667d6fd76bee56
Michael Wood <113477758+mikewood17@users.noreply.github.com>
2022-11-28 21:31:58 +0000

    added js for email validation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// ====================================================
+// Main Javascript
+// ====================================================
+
+
 $('.slides').slick({
     dots: true,
     slidesToShow: 3,
@@ -13,4 +18,49 @@ window.onload = function () {
         menu_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
     })
+}
+
+// form validation
+const form = document.getElementById('form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkInputs();
+});
+function checkInputs() {
+    //get the values from the inputs
+    const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const emailValue = email.value.trim();
+    const subjectValue = subject.value;
+    const messageValue = message.value;
+
+    if(firstNameValue === '') {
+        // show error
+
+        //add error class
+        setErrorFor(firstName, 'first name cannot be blank.');
+    } else {
+        // add success
+        setSuccessFor(firstName);
+    }
+}
+function setErrorFor(input, message) {
+    const contactInput = input.parentElement;
+    const small = contactInput.querySelector('small');
+
+    // add error message inside small tag
+    small.innerText = message;
+
+    // add error class 
+    contactInput.classList.add("error");
+}
+
+function setSuccessFor(input) {
+    const contactInput = input.parentElement;
+    contactInput.classList.add("success");
 }
