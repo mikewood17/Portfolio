5c8d1a2d753282e141f35188ab203d7c5267a8ec
Michael Wood <113477758+mikewood17@users.noreply.github.com>
2022-11-28 23:16:11 +0000

    layout issues resolved

--- js/main.js
+++ js/main.js
@@ -8,9 +8,7 @@ $('.slides').slick({
     slidesToShow: 3,
     slidesToScroll: 3
 });
-
-// window.onload = function () {
-
+// hamburger slideout
     const menu_btn = document.querySelector('.hamburger');
     const mobile_menu = document.querySelector('.mobile-nav');
 
@@ -19,13 +17,36 @@ $('.slides').slick({
         mobile_menu.classList.toggle('is-active');
     });
 
+
+
+//     hamMenu2.addEventListener("click",() =>
+// {
+//     if(
+//     hamMenu2.classList.toggle("active"))
+//     {
+//         openNav();
+//         {document.getElementByClass(".mobile-nav").style.display = "block"};
+//     }
+
+//     else
+//     {
+//         closeNav();
+//         {document.getElementByClass(".mobile-nav").style.display = "none"}
+//     }
+// });
+
+
 // form validation
+
 const form = document.getElementById('form');
 const firstName = document.getElementById('first-name');
 const lastName = document.getElementById('last-name');
+const nameRegex = /[^0-9]/;
 const email = document.getElementById('email');
+// const emailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 const subject = document.getElementById('subject');
 const message = document.getElementById('message');
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     checkInputs();
@@ -38,11 +59,20 @@ function checkInputs() {
     const subjectValue = subject.value;
     const messageValue = message.value;
 
+    // check if empty field
     if(firstNameValue === '') {
         // show error
 
         //add error class
         setErrorFor(firstName, 'first name cannot be blank.');
+    } else {
+        // add success
+        setSuccessFor(firstName);
+    }
+        // check if contains special characters 
+    if(firstNameValue.match(nameRegex)){
+        // add error class
+        setErrorFor(firstName, 'First name cannot contain any special characters');
     } else {
         // add success
         setSuccessFor(firstName);
@@ -56,11 +86,11 @@ function setErrorFor(input, message) {
     small.innerText = message;
 
     // add error class 
-    contactInput.classList.add("error");
+    contactInput.className = 'contactInput error'
 }
 
 function setSuccessFor(input) {
     const contactInput = input.parentElement;
     // add success icon and border
-    contactInput.classList.add("success");
+    contactInput.className = 'contactInput success'
 }
