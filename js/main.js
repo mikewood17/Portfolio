ff1c0dedff95702a942fbbb7a672fe90c76f773a
Michael Wood <113477758+mikewood17@users.noreply.github.com>
2022-12-21 13:13:39 +0000

    finished up the issues i was having with my carousel. Is now working intended

--- js/main.js
+++ js/main.js
@@ -64,6 +64,13 @@ async function typeSentence(sentence, eleRef, delay = 100) {
   });
 
   $('.carousel').slick({
-
-    variableWidth: true
+    arrows: false,
+    slidesToShow: 1,
+    dots: true,
+    variableWidth: true,
+    centerMode: true,
+    centerPadding: '50px',
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear'
   });
